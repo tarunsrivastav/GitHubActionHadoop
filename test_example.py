db6984666b27b7511c48c1aee55e0f0358edd405
Tarun Srivastav <47325839+tarunsrivastav@users.noreply.github.com>
2023-12-08 03:29:59 +0530

    Create test_example.py

---
+++ test_example.py
@@ -0,0 +1,12 @@
+# test_example.py
+import unittest
+from example import add_numbers
+
+class TestExample(unittest.TestCase):
+
+    def test_add_numbers(self):
+        self.assertEqual(add_numbers(2, 3), 5)
+        self.assertEqual(add_numbers(-1, 1), 0)
+
+if __name__ == '__main__':
+    unittest.main()
